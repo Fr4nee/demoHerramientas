6544fb94b3ad91e5392c7db84883fe29329fc5c4
franco romano <franromano99@outlook.com>
2022-08-16 21:36:18 -0300

    Clase 1

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace demoHerramientas.Pages;
+
+public class IndexModel : PageModel
+{
+    [BindProperty]
+    public List<Alumno> Alumnos { get; set; }
+
+
+    //metodo tipo ngOnInit
+    public void OnGet()
+    {
+        List<Alumno> ListadoAlumno = new List<Alumno>{
+            new Alumno{DNI = 12345678, Nombre = "Juan", Apellido = "Perez"},
+            new Alumno{DNI = 12345679, Nombre = "Pedro", Apellido = "Perez"},
+            new Alumno{DNI = 12345680, Nombre = "Maria", Apellido = "Perez"}
+        };
+        Alumnos = ListadoAlumno;
+    }
+
+    public void OnPost() 
+    {
+        
+    }
+
+
+}    
+
+    public class Alumno
+    {
+        public int DNI { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+    }
